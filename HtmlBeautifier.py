a5e213508e58ce6f3f0be8c861de9592b90fd9e9
Micheal Cole Young <coleyoungmusic@gmail.com>
2017-10-20 23:01:24 -0500

    Add files via upload

---
+++ HtmlBeautifier.py
@@ -0,0 +1,33 @@
+from appJar import gui
+from bs4 import BeautifulSoup
+import codecs
+import pyperclip
+
+
+def format(btn):
+    html_file = codecs.open(app.getEntry("html"))
+    read = html_file.read()
+    soup = BeautifulSoup(read, "html.parser")
+    app.setTextArea("formatted", soup.prettify())
+
+
+def clear(btn):
+    app.clearTextArea("formatted")
+    app.clearEntry("html")
+
+
+def copy(btn):
+    pyperclip.copy(app.getTextArea("formatted"))
+
+
+app = gui("HTML BEAUTIFIER", "600x362")
+app.setFont(20)
+app.setGuiPadding(10,10)
+app.setBg("grey")
+app.addEntry("html")
+app.setEntryDefault("html", "Enter file path to HTML document to beautify!")
+app.addTextArea("formatted")
+app.addButtons(["format", "clear", "copy"], [format, clear, copy])
+
+app.go()
+
